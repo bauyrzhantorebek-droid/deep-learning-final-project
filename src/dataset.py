e1e570955a9e22ec22ba3a1dcca48a27b0390fed
bauyrzhantorebek-droid <bauyrzhan.torebek@narxoz.kz>
2026-05-10 22:35:39 +0500

    feat: add custom PyTorch dataset class

---
+++ src/dataset.py
@@ -0,0 +1,33 @@
+import torch
+from torch.utils.data import Dataset
+
+class ToxicCommentDataset(Dataset):
+    def __init__(self, texts, targets, tokenizer, max_len):
+        self.texts = texts
+        self.targets = targets
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, item):
+        text = str(self.texts[item])
+        target = self.targets[item]
+
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            return_token_type_ids=False,
+            padding='max_length',
+            truncation=True,
+            return_attention_mask=True,
+            return_tensors='pt',
+        )
+
+        return {
+            'input_ids': encoding['input_ids'].flatten(),
+            'attention_mask': encoding['attention_mask'].flatten(),
+            'targets': torch.tensor(target, dtype=torch.float)
+        }
